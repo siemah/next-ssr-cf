48c4a6c626281f0e4a6ca087a143d3caf363413a
siemah <hmi.dayenio@gmail.com>
2023-06-26 11:51:49 +0100

    ref:use a whole page ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Work_Sans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Work_Sans({
+  subsets: ['latin'],
+  display: "swap",
+  weight: ["200", "400"]
+});
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,6 +19,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet='utf-8' />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>Tailwind Starter Template - Nordic Shop: Tailwind Toolbox</title>
+        <meta name="description" content="Free open source Tailwind CSS Store template" />
+        <meta name="keywords" content="tailwind,tailwindcss,tailwind css,css,starter template,free template,store template, shop layout, minimal, monochrome, minimalistic, theme, nordic" />
+
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-interface CardProps {
+export interface CardProps {
   image: string;
   name: string;
   shortDescription: string;
